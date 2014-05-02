218c6171c9a924701d9168b88e9d14332022db2c
drbjones <b-jones@kellogg.northwestern.edu>
2014-05-02 16:06:20 -0500

    Solutions to first two tasks

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,6 @@ class MoviesController < ApplicationController
     # Your code goes here
     # Use the information after the slash in the URL to look up the movie with the corresponding ID number
     # Store the movie in the @movie instance variable so the view can format it
-
-    # @movie = ???
+    @movie = Movie.find_by({:id=>params[:id]}).title
   end
 end
